f806ff16f717f0f92b000ce0d4a56120f66dd37f
vova-xolera <sedelnik0v@yandex.ru>
2019-09-18 15:50:39 +0300

    Максимум из двух чисел [#177516]

---
+++ chapter_001/src/test/java/condition/MaxTest.java
@@ -0,0 +1,16 @@
+package condition;
+
+
+import org.junit.Test;
+import ru.job4j.condition.Max;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
