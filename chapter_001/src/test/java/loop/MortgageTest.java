1abac401e1d819347feaa9616c657c7b3e25000b
vova-xolera <sedelnik0v@yandex.ru>
2019-09-19 15:05:39 +0300

    5.7. Ипотека [#177457]

---
+++ chapter_001/src/test/java/loop/MortgageTest.java
@@ -0,0 +1,23 @@
+package loop;
+
+import org.junit.Test;
+import ru.job4j.loop.Mortgage;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MortgageTest {
+    @Test
+    public void when1Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(1000, 1200, 1);
+        assertThat(year, is(1));
+    }
+
+    @Test
+    public void when2Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(100, 120, 50);
+        assertThat(year, is(2));
+    }
+}
