172887bc9c4a8a3c3a8b470ee08fa0c4ec017ecd
vova-xolera <sedelnik0v@yandex.ru>
2019-09-19 16:15:38 +0300

    5.8. JavaDoc. Code convention [#177453]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,30 +1,63 @@
 package ru.job4j.calculator;
 
-import sun.awt.geom.AreaOp;
+/**
+ * Package for calculate task.
+ *
+ * @author Vladimir Sedelnikov (sedelnik0v@yandex.ru)
+ * @version 19.09.2019
+ * @since 1.1
+ */
 
 public class Calculator {
+
+    /**
+     * Сложение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
+
+    /**
+     * Деление
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void div (double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
-
     }
 
+    /**
+     * Умножение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void multiply (double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
-
     }
 
+    /**
+     * Вычитание
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void subtrack (double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
-
     }
 
+    /** Конструктор, проверка работоспособности вышеперчисленных методов
+     *
+     * @param args - args
+     */
     public static void main(String[] args) {
        add(1,2);
        div(3,4);
