86c1d595d09869563f98feb78e6c62ee5d3f4d34
vova-xolera <sedelnik0v@yandex.ru>
2019-09-19 10:56:43 +0300

    Доработка с задачей про стороны треугольника

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,14 +1,10 @@
 package ru.job4j.condition;
 
-import static java.lang.Math.pow;
-
 public class SqArea {
-    public static double square(int p, int k) {
-        return k * pow(p,2) / (4 * (pow(k, 2) + 2 * k + 1));
-    }
-
-    public static void main(String[] args) {
-        double result1 = square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
---
+++ chapter_001/src/test/java/condition/SqAreaTest.java
@@ -0,0 +1,28 @@
+package condition;
+
+import org.junit.Test;
+import ru.job4j.condition.SqArea;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+}
