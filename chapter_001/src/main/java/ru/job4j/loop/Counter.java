afe5e62170aded03823e94c8c14f3cd8166d1043
vova-xolera <sedelnik0v@yandex.ru>
2019-09-19 13:17:06 +0300

    5.1 Ответ на комментарий.

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -4,10 +4,9 @@ public class Counter {
         public static int add(int start, int finish) {
             int sum = 0;
             for (int i = start; i <= finish; i++) {
-                    if (i % 2 != 0) {
-                        continue;
-                     }
-             sum = sum + i;
+                if (i % 2 == 0) {
+                    sum = sum + i;
+                }
             }
             return sum;
         }
