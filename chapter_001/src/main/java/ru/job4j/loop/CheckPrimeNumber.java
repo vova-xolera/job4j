3a8850f5b033888691a6d3d02503e88d769604e6
vova-xolera <sedelnik0v@yandex.ru>
2019-09-19 14:30:32 +0300

    5.5. Простое число [#177458]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,7 +1,7 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
-    public boolean check(int finish) {
+    public static boolean check(int finish) {
             boolean prime = finish != 1;
             for (int i = finish - 1; i > 1; i--) {
                 if (finish % i == 0) {
---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 1; i <= finish; i ++) {
+            if (CheckPrimeNumber.check(i)) count++;
+        }
+        return count;
+    }
+}
