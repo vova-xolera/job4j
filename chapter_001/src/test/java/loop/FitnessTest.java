9b640d69ec83aadea33ee5c02eae0f8ff56569d1
vova-xolera <sedelnik0v@yandex.ru>
2019-09-19 13:08:05 +0300

    5.4 Протеиновая диета. [#177455]

---
+++ chapter_001/src/test/java/loop/FitnessTest.java
@@ -0,0 +1,30 @@
+package loop;
+
+import org.junit.Test;
+import ru.job4j.loop.Fitness;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FitnessTest {
+    @Test
+    public void whenIvanGreatNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(95, 90);
+        assertThat(days, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(90, 95);
+        assertThat(days, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(50, 90);
+        assertThat(days, is(2));
+    }
+}
