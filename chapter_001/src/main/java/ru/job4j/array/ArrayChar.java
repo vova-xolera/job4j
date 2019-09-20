8d0178aaedbd8330d4ebfa9af886bb321c0cb102
vova-xolera <sedelnik0v@yandex.ru>
2019-09-20 11:56:35 +0300

    6.4. Слово начинается с ...[#177464]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+            for (int index = 0; index < pref.length; index++){
+                if (pref[index] != word[index]) {
+                  result = false;
+                  continue;
+                 }
+                else if (pref[index] == word[index]) {
+                  result = true;
+                }
+            }
+        return result;
+    }
+}
