9c912d303668923b08de5be3725120ce569a3fcd
vova-xolera <sedelnik0v@yandex.ru>
2019-09-20 12:17:23 +0300

    6.4.1. Слово заканчивается с ...[#177472]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        for (int index = 0; index < post.length; index++){
+            if (post[index] != word[word.length - post.length + index]) {
+                result = false;
+                continue;
+            }
+            else if (post[index] == word[word.length - post.length + index]) {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
