0413a092d5fd4ba36ec7ae7ad2f4826038febd38
vova-xolera <sedelnik0v@yandex.ru>
2019-09-20 09:54:00 +0300

    6.0. Заполнить массив степенями чисел.[#177461]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+import static java.lang.StrictMath.pow;
+
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for(int i = 0; i < bound; i++) {
+           rst[i] = (int) pow(i + 1,2);
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/array/SquareTest.java
@@ -0,0 +1,18 @@
+package array;
+
+import org.junit.Test;
+import ru.job4j.array.Square;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+}
