33151aee6ca365dba225855a03e5237f56cee53b
vova-xolera <sedelnik0v@yandex.ru>
2019-09-22 11:20:16 +0300

    6.4. Слово начинается с ...[#177464] Комментарий

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,10 +2,10 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true;
             for (int index = 0; index < pref.length; index++){
-                if (pref[index] == word[index]) {
-                  result = true;
+                if (pref[index] != word[index]) {
+                  result = false;
                   break;
                  }
             }
