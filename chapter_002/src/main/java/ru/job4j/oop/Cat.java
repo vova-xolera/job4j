d15d96f962a6a4e339be6465a38041bbf75c5f61
Vladimir Sedelnikov <sedelnik0v@yandex.ru>
2019-09-29 15:19:10 +0300

    1.4. Переводчик Русский-Английский.[#177585]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -2,14 +2,31 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
     }
 }
--- chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -1,10 +1,11 @@
 package ru.job4j.oop;
 
 public class DummyDic {
-    public static String engToRus(String eng) {
+    public  String engToRus(String eng) {
         return "Неизвестное слово. " + eng;
     }
     public static void main(String[] args) {
-    System.out.println(DummyDic.engToRus("Word"));
+        DummyDic dic = new DummyDic();
+    System.out.println(dic.engToRus("Word"));
     }
 }
