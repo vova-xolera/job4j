f232082b254779b98b678b9b8f9537474db9b8e0
Vladimir Sedelnikov <sedelnik0v@yandex.ru>
2019-09-30 14:00:39 +0300

    3. Реализация профессий в коде[#177589]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/professions/Builder.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker.professions;
+
+public class Builder extends Programmer {
+    private int ConstructedBuilding;
+    public int GetConstructedBuilding() {
+        return ConstructedBuilding;
+    }
+}
