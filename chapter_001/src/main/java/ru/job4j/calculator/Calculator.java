8f2b975aa577138151e173285fd91a15f67a222d
Vladimir Sedelnikov <sedelnik0v@yandex.ru>
2019-09-30 10:23:47 +0300

    3. Перегрузить метод max для трех чисел[#177578]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,60 +1,15 @@
 package ru.job4j.calculator;
-/**
- * Package for calculate task.
- *
- * @author Vladimir Sedelnikov (sedelnik0v@yandex.ru)
- * @version 19.09.2019
- * @since 1.1
- */
+
 public class Calculator {
-    /**
-     * Сложение
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
-    public static void add(double first, double second) {
-        double result = first + second;
-        System.out.println(first + " + " + second + " = " + result);
+
+    public double add(double first, double second) {
+        return first + second;
     }
-    /**
-     * Деление
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
-    public static void div(double first, double second) {
-        double result = first / second;
-        System.out.println(first + " / " + second + " = " + result);
-    }
-    /**
-     * Умножение
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
-    public static void multiply(double first, double second) {
-        double result = first * second;
-        System.out.println(first + " * " + second + " = " + result);
-    }
-    /**
-     * Вычитание
-     * @param first первый аргумент
-     * @param second второй аргумент
-     * @return результат
-     */
-    public static void subtrack(double first, double second) {
-        double result = first - second;
-        System.out.println(first + " - " + second + " = " + result);
-    }
-    /** Конструктор, проверка работоспособности вышеперчисленных методов
-     *
-     * @param args - args
-     */
-    public static void main(String[] args) {
-       add(1, 2);
-       div(3, 4);
-       multiply(5, 6);
-       subtrack(7, 8);
+
+    public double add(double first, double second, double third) {
+        return add(
+                first,
+                add(second, third)
+        );
     }
 }
--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,16 @@
 package ru.job4j.condition;
 
 public class Max {
-        public static int max(int left, int right) {
-            int result = left <= right ? right : left;
+        public static int max(int first, int second) {
+            int result = first <= second ? second : first;
+            return result;
+        }
+        public static int max(int first, int second, int third) {
+            int result = first <= max(second, third) ? max(second, third) : first;
+            return result;
+        }
+        public static int max(int first, int second, int third, int forth) {
+            int result = max(second, first) <= max(third, forth) ? max(third, forth) : max(second, first);
             return result;
         }
 }
