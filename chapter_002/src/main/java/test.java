076335cf95203eb41e403798cba28bc476369a23
Vladimir Sedelnikov <sedelnik0v@yandex.ru>
2020-02-19 21:45:15 +0300

    4.3. Компаратор для строк.[#239605]

--- chapter_002/src/main/java/test.java
+++ chapter_002/src/main/java/test.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
+    public static void main(String[] args) {
+        List<Integer> list =  new ArrayList<>();
+        for (int i = 0; i < 109; i++) {
+            list.add(i);
+        }
+        int size = (int) Math.round((double)list.size()/2);
+        for (int i = 0; i < size; i++) {
+            list.remove(i);
+        }
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
 }
