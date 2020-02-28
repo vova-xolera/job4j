df3fc2e7c2c78b7b4e63f4ef57274f73e091c41f
Vladimir Sedelnikov <sedelnik0v@yandex.ru>
2020-02-28 15:54:48 +0300

    new Chapter

--- chapter_004/src/test/java/ru/job4j/lambda/FunctionInDiapasonTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionInDiapasonTest.java
@@ -8,21 +8,21 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+
 
 public class FunctionInDiapasonTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
         List<Double> result = FunctionInDiapason.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
-        assertThat(result, is(expected));
+        Assert.assertThat(result, is(expected));
     }
 
     @Test
     public void whenQuadraticFunctionThenQuadraticResults() {
         List<Double> result = FunctionInDiapason.diapason(5, 8, x -> x * x + 1);
         List<Double> expected = Arrays.asList(26D, 37D, 50D);
-        assertThat(result, is(expected));
+        Assert.assertThat(result, is(expected));
     }
 
     @Test
